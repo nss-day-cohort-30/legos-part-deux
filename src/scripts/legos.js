9a049c19f90f192dde535f28da5d8dd9487831c4
Steve Brownlee <chortlehoort@gmail.com>
2019-02-04 11:10:00 -0600

    Now rendering color name in HTML representation instead of Id

--- src/scripts/legos.js
+++ src/scripts/legos.js
@@ -8,40 +8,50 @@ const createColorOption = color => `
     <option value="${color.id}">${color.color}</option>
 `
 
-const createLegoElement = lego => `
-    <section class="lego">
-        <h1>${lego.creation}</h1>
+const createLegoElement = (lego, colors) => {
+    const iFoundYou = colors.find(c => parseInt(c.id) === lego.color)
+    return `
 
-        <div>Shape: ${lego.shape}</div>
-        <div>Color: ${lego.color}</div>
-        <div>Creator: ${lego.creator}</div>
-    </section>
-`
+        <section class="lego">
+            <h1>${lego.creation}</h1>
 
-const getAllLegos = () =>
+            <div>Shape: ${lego.shape}</div>
+            <div>Color: ${iFoundYou.color}</div>
+            <div>Creator: ${lego.creator}</div>
+        </section>
+    `
+}
+
+const getAllLegos = (colors) =>
     fetch("http://127.0.0.1:8088/legos")
         .then(r => r.json())
         .then(legoArray => {
             $("#legoList").innerHTML = ""
             legoArray.forEach(
                 (lego) => {
-                    const html = createLegoElement(lego)
+                    const html = createLegoElement(lego, colors)
                     $("#legoList").innerHTML += html
                 }
             )
         })
 
+let allColors = []
 
-fetch("http://127.0.0.1:8088/colors")
-    .then(r => r.json())
-    .then(colorArray => {
-        colorArray.forEach(
-            (color) => {
-                const html = createColorOption(color)
-                $("#lego__color").innerHTML += html
-            }
-        )
-    })
+const getAllColors = () => {
+    return fetch("http://127.0.0.1:8088/colors")
+        .then(r => r.json())
+        .then(colorArray => {
+            allColors = colorArray
+            colorArray.forEach(
+                (color) => {
+                    const html = createColorOption(color)
+                    $("#lego__color").innerHTML += html
+                }
+            )
+            return colorArray
+        })
+
+}
 
 $(".lego__save").addEventListener("click", event => {
   const creator = $("#lego__creator").value;
@@ -73,13 +83,17 @@ $(".lego__save").addEventListener("click", event => {
                 I am 100% certain the POST operation
                 was successful
             */
-            getAllLegos()
+            getAllLegos(allColors)
         }
     )
 })
 
+getAllColors().then(
+    (colorArray) => {
+        getAllLegos(colorArray)
+    }
+)
 
-getAllLegos()
 
 
 
